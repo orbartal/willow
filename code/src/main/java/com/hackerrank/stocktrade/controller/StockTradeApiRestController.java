5ce4c91af495844122f901ccae875913137c5c5e
OR Bartale <orbartal@gmail.com>
2020-08-01 16:49:05 -0400

    Add controller methods as stubs

--- code/src/main/java/com/hackerrank/stocktrade/controller/StockTradeApiRestController.java
+++ code/src/main/java/com/hackerrank/stocktrade/controller/StockTradeApiRestController.java
@@ -1,22 +1,62 @@
 package com.hackerrank.stocktrade.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import com.hackerrank.stocktrade.controller.model.response.StockPricesResponse;
+import com.hackerrank.stocktrade.controller.model.response.StockStateResponse;
+import com.hackerrank.stocktrade.model.Trade;
 
 @RestController
 public class StockTradeApiRestController {
-    public StockTradeApiRestController() {
-        ArrayList<String> trades = new ArrayList<String>();
-        ArrayList<String> users = new ArrayList<String>();
+
+    //Adding new trades
+    @RequestMapping(value = "/trades", method = RequestMethod.POST)
+	public ResponseEntity<Void> addTrade(@RequestBody Trade trade) throws RuntimeException {
+    	return ResponseEntity.ok().build();	
     }
 
-    private ArrayList<String> trades;
-    private ArrayList<String> users;
-
+	//Erasing all the trades
     @DeleteMapping("/erase")
-    void erase() {
-        trades.clear();
+    public ResponseEntity<Void> erase() {
+    	return ResponseEntity.ok().build();	
     }
+
+    //Returning all the trades
+    @RequestMapping(value = "/trades", method = RequestMethod.GET)
+	public ResponseEntity<List<Trade>> getAllTrades() throws RuntimeException {
+    	return ResponseEntity.ok(new ArrayList<>());	
+	}
+    
+    //Returning the trade records filtered by the user ID
+    @RequestMapping(value = "/trades/users/{userID}", method = RequestMethod.GET)
+	public ResponseEntity<List<Trade>> getAllTradesByUser(@PathVariable("userID") Long userID) throws RuntimeException {
+    	return ResponseEntity.ok(new ArrayList<>());	
+	}
+    
+    //Returning the highest and lowest price for the stock symbol in the given date range
+    @RequestMapping(value = "/stocks/{stockSymbol}/price?start={startDate}&end={endDate}", method = RequestMethod.GET)
+	public ResponseEntity<StockPricesResponse> getStocksPricesByDateRange(
+			@PathVariable("startDate") Date startDate,
+			@PathVariable("endDate") Date endDate) throws RuntimeException {
+    	return ResponseEntity.ok(new StockPricesResponse());	
+	}
+    
+    //Returning the fluctuations count, maximum daily rise and maximum daily fall for each stock symbol for the period in the given date range
+    @RequestMapping(value = "/stocks/stats?start={startDate}&end={endDate}", method = RequestMethod.GET)
+	public ResponseEntity<StockStateResponse> getStocksStatsByDateRange(
+			@PathVariable("startDate") Date startDate,
+			@PathVariable("endDate") Date endDate) throws RuntimeException {
+    	return ResponseEntity.ok(new StockStateResponse());	
+	}
+
 }
---
+++ code/src/main/java/com/hackerrank/stocktrade/controller/model/response/StockStateResponse.java
@@ -0,0 +1,35 @@
+package com.hackerrank.stocktrade.controller.model.response;
+
+public class StockStateResponse {
+
+	private String symbol; // The stock symbol for the requested stock.
+	private int fluctuations; // This field describes the number of fluctuations
+	private Double maxRise; // This field is the maximum daily price increase for the period.
+	private Double maxFall; // This field is the maximum daily price decline for the period.
+
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public int getFluctuations() {
+		return fluctuations;
+	}
+	public void setFluctuations(int fluctuations) {
+		this.fluctuations = fluctuations;
+	}
+	public Double getMaxRise() {
+		return maxRise;
+	}
+	public void setMaxRise(Double maxRise) {
+		this.maxRise = maxRise;
+	}
+	public Double getMaxFall() {
+		return maxFall;
+	}
+	public void setMaxFall(Double maxFall) {
+		this.maxFall = maxFall;
+	}
+
+}
